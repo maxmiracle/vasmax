f0d648ed8e395aecdbe5d4fe367d15205f734887
maxmiracle <maxmiracle>
2023-11-13 00:30:39 +0000

    Initial commit

---
+++ src/test/java/com/vasmax/VasmaxTest.java
@@ -0,0 +1,21 @@
+package com.vasmax;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class VasmaxTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
